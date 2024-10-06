d3592eceb7c98e5a7f03fe932fdc1fc9bac5d28a
Sumit Kumar Shaw <sumitshaw8274060134@gmail.com>
2024-10-06 15:23:07 +0530

    Modified path

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let currFolder
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`${folder}/`)
     let response = await a.text()
 
     let div = document.createElement("div")
@@ -71,7 +71,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`songs/`)
     let cardContainer = document.querySelector(".cardContainer")
     let response = await a.text()
     let div = document.createElement("div")
@@ -83,7 +83,7 @@ async function displayAlbums() {
         const e = array[index]
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").splice(-2)[0]
-            let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
